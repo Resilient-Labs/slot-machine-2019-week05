78928a65825c75715f3a5561e691211017d50072
roxanalemus <roxanalemusdev@gmail.com>
2021-09-17 12:24:13 -0400

    theme update: background, fonts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*create 3 box slot machine
 requirements:
+five options per reel
 min button: $5, max button: $50
 player starts at $1000;
 if player gets all three matching, player wins 10 times what they bet
@@ -24,17 +25,18 @@ function playerBetsFive(){
         leftBox.textContent = "";
         middleBox.textContent = "";
         rightBox.textContent = "";
-        return message.textContent = "All the losers get stickers!"
+        return message.textContent = "later days"
     }
 
-   leftBox.textContent = Math.ceil(Math.random() *3);
-   middleBox.textContent = Math.ceil(Math.random() *3);
-   rightBox.textContent = Math.ceil(Math.random() *3);
+   leftBox.textContent = Math.ceil(Math.random() *5);
+   middleBox.textContent = Math.ceil(Math.random() *5);
+   rightBox.textContent = Math.ceil(Math.random() *5);
     if(leftBox.textContent === middleBox.textContent && leftBox.textContent === rightBox.textContent){
         wallet.textContent = parseInt(wallet.textContent) + (5*10);
-        return message.textContent = "Rich people run funny. Must be all the money in their pockets."
+        return message.textContent = "groovy"
     }else{
         wallet.textContent = parseInt(wallet.textContent) - 5;
+        return message.textContent = "bummer";
     }
 }
 
@@ -43,17 +45,18 @@ function playerBetsFifty(){
         leftBox.textContent = "";
         middleBox.textContent = "";
         rightBox.textContent = "";
-        return message.textContent = "All the losers get stickers!"
+        return message.textContent = "later days"
     }
 
-    leftBox.textContent = Math.ceil(Math.random() *3);
-    middleBox.textContent = Math.ceil(Math.random() *3);
-    rightBox.textContent = Math.ceil(Math.random() *3);
+    leftBox.textContent = Math.ceil(Math.random() *5);
+    middleBox.textContent = Math.ceil(Math.random() *5);
+    rightBox.textContent = Math.ceil(Math.random() *5);
     if(leftBox.textContent === middleBox.textContent && leftBox.textContent === rightBox.textContent){
         wallet.textContent = parseInt(wallet.textContent) + (50*10);
-        return message.textContent = "Rich people run funny. Must be all the money in their pockets.";
+        return message.textContent = "out of sight";
     }else{
         wallet.textContent = parseInt(wallet.textContent) - 50;
+        return message.textContent = "hertz donut";
     }
 }
 
@@ -62,22 +65,4 @@ betFive.addEventListener("click", playerBetsFive);
 betFifty.addEventListener("click", playerBetsFifty);
 
 
-/*document.querySelectorAll("button").forEach(element=>element.addEventListener("click", boxSpin))
-let chances = ["pink", "purple", "black", "red", "lightgray"];
-*/
-/*function boxSpin(){
-//console.log(randomNumber);
-    for( let i = 0; i < 3; i++ ){
-        let randomNumber = Math.floor(Math.random()*chances.length);
-        if( i == 0 ){
-            leftBox.style.backgroundColor = chances[randomNumber]
-        }else if( i == 1){
-            middleBox.style.backgroundColor = chances[randomNumber]
-        }else{
-            rightBox.style.backgroundColor = chances[randomNumber]
-        }
-    }
-}
-*/
 
-//pick one color from array and pass it into each box
