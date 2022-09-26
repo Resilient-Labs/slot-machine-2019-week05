fa8a757dfa2487b117abb2b56d12cec090871683
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-09-25 20:36:14 -0400

    finished project

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let bankBalance = 1000
 
 function playGame() {
 
-let myArray = ['<img src="https://static01.nyt.com/images/2019/04/16/sports/16onsoccerweb-2/merlin_153612873_5bb119b9-8972-4087-b4fd-371cab8c5ba2-superJumbo.jpg?quality=75&auto=webp" alt="">', '<img src="https://imageio.forbes.com/specials-images/imageserve/62aa51710c3e65f16ed3b373/Manchester-United-v-Atalanta--Group-F---UEFA-Champions-League/0x0.jpg?format=jpg&crop=1928,1315,x214,y48,safe&width=960" alt="">', '<img src="https://qph.cf2.quoracdn.net/main-qimg-20c935639fcd9792f35712debf86726f.webp" alt="">', '<img src="https://cdn.britannica.com/72/139472-050-E733BAAF/Xavi-FC-Barcelona-football-match-2009.jpg" alt="">', '<img src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blte9d1a15b99db144b/60db23cb22d24e38a388af70/a4db7e31ab71857fa99b25a6d39744fa273e17fa.png" alt="">']
+let myArray = ['<img src="images/cherry.png" alt="">', '<img src="images/apple.png" alt="">', '<img src="images/seven.png" alt="">', '<img src="images/slot-machine.png" alt="">', '<img src="images/watermelon.png" alt="">']
 
 let result1 = document.querySelector('#result1')
 let img1 = document.querySelector('#img1')
