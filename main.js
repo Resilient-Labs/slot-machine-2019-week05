e5d5135f6b0aebaa98de8fb587b738ba1a7337f9
Randi Crews <randireneecrews@gmail.com>
2023-03-16 13:52:03 -0400

    got rid of play button and bets can no longer stack

--- main.js
+++ main.js
@@ -1,44 +1,76 @@
 // establish bank
-
-// player clicks button
-document.getElementById("play").addEventListener('click', displaySymbols)
+let playerBank = 1000
+let betAmount = 0
+// player makes low or high bet
+document.getElementById("bet10").addEventListener('click', take10)
+document.getElementById("bet50").addEventListener('click', take50)
 // money leaves bank
 
 // generate a random symbol 
-function genSymbol(slotValue){
-    let randomNumber = Math.random()
-    let symbolShown = "cherries"
-    if(randomNumber < .15 ){ 
+function genSymbol(){
+    let randomNumber = Math.random()*100
+    let symbolShown = "start"
+    if(randomNumber < 100 && randomNumber > 95 ){ // rare
         symbolShown = "wild"
-    } if (randomNumber < .35 ){ 
-        symbolShown = "bars"
-    } if (randomNumber < .55 ){ 
-        symbolShown = "bell"
-    } if (randomNumber < .75 ){ 
+    } if (randomNumber < 96 && randomNumber > 80 ){
         symbolShown = "sevens"
+    } if (randomNumber < 81 && randomNumber > 64 ){ 
+        symbolShown = "bar"
+    } if (randomNumber < 65 && randomNumber > 32 ){ 
+        symbolShown = "bell"
+    } if (randomNumber >= 0 && randomNumber < 33 ){
+        symbolShown = "cherries"
     }
-    slotValue = symbolShown
+    return symbolShown 
 }
-// three times
-
 // if u can make a couple options show before the image settles that'd be cool
 
 // display symbols on dom
 function displaySymbols(){
-    let slot1Display = document.getElementById("slot1")
-    let slot2Display = document.getElementById("slot2")
-    let slot3Display = document.getElementById("slot3")
-
-    genSymbol(slot1)
-    genSymbol(slot2)
-    genSymbol(slot3)
-
-    slot1Display
-
-
-
-
+    let slot1Display = genSymbol()
+    let slot2Display = genSymbol()
+    let slot3Display = genSymbol()
+    document.getElementById("slot1").innerText = slot1Display
+    document.getElementById("slot2").innerText = slot2Display
+    document.getElementById("slot3").innerText = slot3Display
+    checkMatch()
 }
-// do symbols match?
 
+// do symbols match?
+function checkMatch(){
+    let slot1Display = document.getElementById("slot1").innerText
+    let slot2Display = document.getElementById("slot2").innerText
+    let slot3Display = document.getElementById("slot3").innerText 
+    document.getElementById("playerBank").innerText = playerBank
+    let betAmount = Number(document.getElementById("betAmount").innerText)
+    if(slot1Display === slot2Display && slot2Display === slot3Display){
+    playerBank = playerBank + (betAmount * 100)
+    alert('YOU\'RE A WINNER HONEY!')
+    }
+}
 // if player wins add money to bank
+
+function take10(){
+    document.getElementById("betAmount").innerText = betAmount
+    document.getElementById("playerBank").innerText = playerBank
+    if (playerBank >= 10){
+        betAmount = 10
+        playerBank -= 10
+        displaySymbols()
+    } else {
+        alert('WITH WHAT MONEY?!')
+    }
+}
+// player makes high bet
+
+function take50(){
+    document.getElementById("betAmount").innerText = betAmount
+    document.getElementById("playerBank").innerText = playerBank
+    if (playerBank >= 50){
+        betAmount = 50
+        playerBank -= 50
+        displaySymbols()
+    } else {
+        alert('WITH WHAT MONEY?!')
+    }
+}
