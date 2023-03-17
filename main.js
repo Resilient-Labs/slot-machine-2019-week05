3db3f88cbcdcc16772f571a12c6fbb9a5e180e68
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-03-17 12:17:11 -0400

    final functionality commit

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const images = document.querySelector('#slot-img')
-let icons = ['diamond', 'horseshoe', 'bar', 'seven', 'lemon']
 let bread = 1000
 let bet = 0
+let currentBalance = document.querySelector('#balance').innerText = bread
+let betTotal = document.querySelector('#betTotal')
+let balanceTotal = document.querySelector('#balance')
+let icons = ['diamond', 'horseshoe', 'bar', 'seven', 'lemon']
 document.querySelector('#spin').addEventListener('click',insertSlot)
 document.querySelector('#bet10').addEventListener('click', ()=>accumulateBet(10))
 document.querySelector('#bet100').addEventListener('click', ()=>accumulateBet(100))
 
 function insertSlot(){
+
+    if(bread > bet){
     let slotOne = Math.round(Math.random()*(4-0)+0)
     let slotTwo = Math.round(Math.random()*(4-0)+0)
     let slotThree = Math.round(Math.random()*(4-0)+0)
@@ -22,25 +27,33 @@ function insertSlot(){
     placeBet(bet)
     winCondiiton(slotOne, slotTwo, slotThree)
     return bet = 0
+    }else{
+        console.log('seek help')
+    }
 }
 
 function placeBet(bet){
-    console.log(bet)
-    console.log(bread)
-    return bread -= bet
+    bread -= bet
+    updatedBread()
 }
-
+function updatedBread(){
+    balanceTotal.innerText = bread
+}
 function accumulateBet(betAmount){
-    return bet += betAmount
+    bet += betAmount
+    updatedBet()
 }
-
+function updatedBet(){
+    betTotal.innerText = bet
+    balanceTotal.innerText = bread - bet
+}
  function winCondiiton(slotOne,slotTwo,slotThree){
     if(slotOne === slotTwo && slotOne === slotThree){
         console.log('jackpot')
-        return bread += ( bet * 3 )
+        return balanceTotal.innerText = bread += ( bet * 3 )
     }else if(slotOne === slotTwo || slotTwo === slotThree){
-        
-        return bread += ( bet * 1.5 )
+        console.log('minor win')
+        return balanceTotal.innerText = bread += ( bet * 1.5 )
     }else{
         console.log('You Lose')
     }
