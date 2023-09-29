5bc9b090c435adf351b2260498dbb779f8c85acd
RoReeceDev <roreecedev@gmail.com>
2023-09-29 12:29:39 -0400

    JS complete for slot machine

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //Create the starting pot amount for player and show that amount in the DOM
 
-let moneyPot = 100;
+let moneyPot = 500;
 document.querySelector('#heartchamber').innerHTML = moneyPot
 
 let slotOne = document.querySelector('#slotone')
 let slotTwo = document.querySelector('#slottwo')
 let slotThree = document.querySelector('#slotthree')
 
-let angelArray =['<img src = "images/atsuko.jpg"></img>','<img src = "images/Canary.jpg"></img>', '<img src = "images/michiko.jpg"></img>', '<img src = "images/miyuki.jpg"></img>', '<img src = "images/Yoruichi.jpg"></img>' ]
+let angelArray =['<img src = "images/atsuko.jpg">','<img src = "images/Canary.jpg">', '<img src = "images/michiko.jpg">', '<img src = "images/miyuki.jpg"></img>', '<img src = "images/Yoruichi.jpg"></img>' ]
 
 
 //Create event listeners for when player clicks the betting amount it runs one of the functions
@@ -18,56 +18,63 @@ document.getElementById('allhearts').addEventListener('click', allHearts)
 
 function fiveHearts(){
     //Update the amount of money in the pot after player makes bet
-    moneyPot = moneyPot - 5;
-    document.querySelector('#heartchamber').innerHTML = moneyPot
-    getRandomAngel()
     if(moneyPot < 5){
-        moneyPot = 0;
-        document.querySelector('#heartchamber').innerHTML = moneyPot
         document.getElementById('winlose').innerText = 'Not Enough Funds'
-    } else if(slotOne === slotTwo && slotOne === slotThree){
-        moneyPot = moneyPot + 50;
+    } else if(moneyPot >= 5){
+        moneyPot = moneyPot - 5;
         document.querySelector('#heartchamber').innerHTML = moneyPot
-        document.getElementById('winlose').innerText = 'You Win'
+        getRandomAngel()
+        if(slotOne === slotTwo && slotOne === slotThree){
+            moneyPot = moneyPot + 50;
+            document.querySelector('#heartchamber').innerHTML = moneyPot
+            document.getElementById('winlose').innerText = 'You Win'
+        } else {
+            document.getElementById('winlose').innerText = 'You Lose'
+        }
+                
+    
     }
-    else {
-        document.getElementById('winlose').innerText = 'You Lose'
-    }
-
 }
 
 function fiftyHearts(){
     //Update the amount of money in the pot after player makes bet
-    moneyPot = moneyPot - 50;
-    document.querySelector('#heartchamber').innerHTML = moneyPot
-    getRandomAngel()
-    if(moneyPot < 50 && moneyPot >= 5){
-        document.getElementById('winlose').innerText = 'Not Enough Funds For This Bet'
-    } else if(slotOne === slotTwo && slotOne === slotThree){
-        moneyPot = moneyPot + 150;
+    if(moneyPot < 50){
+        document.getElementById('winlose').innerText = 'Not Enough Funds'
+    } else if(moneyPot >= 50){
+        moneyPot = moneyPot - 50;
         document.querySelector('#heartchamber').innerHTML = moneyPot
-        document.getElementById('winlose').innerText = 'You Win'
-    }
-    else {
-        document.getElementById('winlose').innerText = 'You Lose'
+        getRandomAngel()
+        if(slotOne === slotTwo && slotOne === slotThree){
+            moneyPot = moneyPot + 100;
+            document.querySelector('#heartchamber').innerHTML = moneyPot
+            document.getElementById('winlose').innerText = 'You Win'
+        } else {
+            document.getElementById('winlose').innerText = 'You Lose'
+        }
+                
+    
     }
 
 }
 
 function allHearts(){
     //Update the amount of money in the pot after player makes bet
-    moneyPot = moneyPot - moneyPot;
-    document.querySelector('#heartchamber').innerHTML = moneyPot
-    getRandomAngel()
-    if(moneyPot < 50 && moneyPot >= 5){
-        document.getElementById('winlose').innerText = 'Not Enough Funds For This Bet'
-    } else if(slotOne === slotTwo && slotOne === slotThree){
-        moneyPot = moneyPot + 500;
+    if(moneyPot <= 0){
+        document.getElementById('winlose').innerText = 'Not Enough Funds'
+    } else if(moneyPot > 0){
+        let currentPot = moneyPot
+        moneyPot = moneyPot - currentPot;
         document.querySelector('#heartchamber').innerHTML = moneyPot
-        document.getElementById('winlose').innerText = 'You Win'
-    }
-    else {
-        document.getElementById('winlose').innerText = 'You Lose'
+        getRandomAngel()
+        if(slotOne === slotTwo && slotOne === slotThree){
+            moneyPot = currentPot * 2;
+            document.querySelector('#heartchamber').innerHTML = moneyPot
+            document.getElementById('winlose').innerText = 'YOU WIN THE JACKPOT'
+        } else {
+            document.getElementById('winlose').innerText = 'You Lose'
+        }
+                
+    
     }
     //
 }
