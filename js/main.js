5df946c6fde21b1b0be7898ddcd18983b5e19608
RoReeceDev <roreecedev@gmail.com>
2023-09-30 14:55:34 -0400

    CSS completed for project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Create the starting pot amount for player and show that amount in the DOM
 
-let moneyPot = 500;
-document.querySelector('#heartchamber').innerHTML = moneyPot
+let moneyPot = 1000;
+document.querySelector('#heartchamber').innerHTML = 'Heart Chamber:' + ' ' + moneyPot
 
 let slotOne = document.querySelector('#slotone')
 let slotTwo = document.querySelector('#slottwo')
@@ -22,11 +22,11 @@ function fiveHearts(){
         document.getElementById('winlose').innerText = 'Not Enough Funds'
     } else if(moneyPot >= 5){
         moneyPot = moneyPot - 5;
-        document.querySelector('#heartchamber').innerHTML = moneyPot
+        document.querySelector('#heartchamber').innerHTML = 'Heart Chamber:' + ' ' + moneyPot
         getRandomAngel()
         if(slotOne === slotTwo && slotOne === slotThree){
-            moneyPot = moneyPot + 50;
-            document.querySelector('#heartchamber').innerHTML = moneyPot
+            moneyPot = moneyPot + 150;
+            document.querySelector('#heartchamber').innerHTML = 'Heart Chamber:' + ' ' + moneyPot
             document.getElementById('winlose').innerText = 'You Win'
         } else {
             document.getElementById('winlose').innerText = 'You Lose'
@@ -42,11 +42,11 @@ function fiftyHearts(){
         document.getElementById('winlose').innerText = 'Not Enough Funds'
     } else if(moneyPot >= 50){
         moneyPot = moneyPot - 50;
-        document.querySelector('#heartchamber').innerHTML = moneyPot
+        document.querySelector('#heartchamber').innerHTML = 'Heart Chamber:' + ' ' + moneyPot
         getRandomAngel()
         if(slotOne === slotTwo && slotOne === slotThree){
-            moneyPot = moneyPot + 100;
-            document.querySelector('#heartchamber').innerHTML = moneyPot
+            moneyPot = moneyPot + 200;
+            document.querySelector('#heartchamber').innerHTML =  'Heart Chamber:' + ' ' + moneyPot
             document.getElementById('winlose').innerText = 'You Win'
         } else {
             document.getElementById('winlose').innerText = 'You Lose'
@@ -59,19 +59,27 @@ function fiftyHearts(){
 
 function allHearts(){
     //Update the amount of money in the pot after player makes bet
+
+    //If there is no money in the pot make it so that the user cannot run the slot machine and it displays no funds availiable 
     if(moneyPot <= 0){
         document.getElementById('winlose').innerText = 'Not Enough Funds'
+
+        //if the pot has money then allow user to bet all their hearts CURRENTLY in the pot and run the function to randomize the angels
     } else if(moneyPot > 0){
         let currentPot = moneyPot
         moneyPot = moneyPot - currentPot;
-        document.querySelector('#heartchamber').innerHTML = moneyPot
+        document.querySelector('#heartchamber').innerHTML = 'Heart Chamber:' + ' ' + moneyPot
         getRandomAngel()
+
+        //if the angels match then it should player will hit the jackpot doubling the amount they played
         if(slotOne === slotTwo && slotOne === slotThree){
             moneyPot = currentPot * 2;
-            document.querySelector('#heartchamber').innerHTML = moneyPot
-            document.getElementById('winlose').innerText = 'YOU WIN THE JACKPOT'
+            document.querySelector('#heartchamber').innerHTML = 'Heart Chamber:' + ' ' + moneyPot
+            document.getElementById('winlose').innerText = 'YOU WIN THE ANGELS\' HEARTS'
+
+        // if angels dont match, player loses, and they're heartbroken
         } else {
-            document.getElementById('winlose').innerText = 'You Lose'
+            document.getElementById('winlose').innerText = 'Heartbroken'
         }
                 
     
@@ -79,6 +87,8 @@ function allHearts(){
     //
 }
 
+
+//create a function tow randomize the angel images that are saved to the angelArray. Then place randomized image in respective slot div
 
 function getRandomAngel(){
     slotOne = angelArray[Math.floor(Math.random() * angelArray.length)]
